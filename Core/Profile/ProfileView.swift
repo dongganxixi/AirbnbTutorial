20569dd64ba62d14d700c75866cb49b246ba87a5
dongganxixi <ruixing.xixi@gmail.com>
2024-06-27 11:13:36 +0800

    增加一个profileview的视图，用于登录界面

---
+++ Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  AirbnbTutorial
+//
+//  Created by 胡瑞兴 on 2024/6/27.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
